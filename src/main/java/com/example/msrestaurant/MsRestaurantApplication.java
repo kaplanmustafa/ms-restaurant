8fe17102f6c3885fa0b9cca539fc2674221502c6
muskaplan <muskaplan@innova.com.tr>
2022-01-02 17:45:02 +0300

    error config

--- src/main/java/com/example/msrestaurant/MsRestaurantApplication.java
+++ src/main/java/com/example/msrestaurant/MsRestaurantApplication.java
@@ -1,13 +1,16 @@
 package com.example.msrestaurant;
 
+import com.example.mscommon.error.ErrorConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import({ErrorConfig.class})
 public class MsRestaurantApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MsRestaurantApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MsRestaurantApplication.class, args);
+    }
 
 }
